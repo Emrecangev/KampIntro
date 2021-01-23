6bd886531298a561e67cee220a94386951cecdc7
emrecangevrek@gmail.com <emrecangevrek@gmail.com>
2021-01-23 18:02:38 +0300

    20.01.2021 Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Emre", "Eda", "Merve" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+
+            // Ekstra eleman ekleme yapılabilir hata vermez. Değer kaybetmenin önüne geçilir. 
+
+            List<string> isimler2 = new List<string> { "Emre", "Eda", "Merve" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("Emrecan");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
